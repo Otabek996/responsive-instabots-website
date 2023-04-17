417185874da14e87eae2a41d3a26a4531665e257
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-04-17 20:15:27 +0500

    Added scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,9 +23,6 @@ function linkAction () {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-
-
-
 navLink.forEach(m => m.addEventListener('click', () => {
     navLink.forEach(b => b.classList.remove('active-link'));
     m.classList.toggle('active-link');
@@ -39,3 +36,23 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     });
 });
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive () {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
